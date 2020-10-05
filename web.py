d523230a8bab1283778c9cca6f4d8d8f6640acd9
surabhi nanhe <nanhe.surabhi@gmail.com>
2020-10-05 12:01:03 +0530

    first commit

---
+++ web.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    #title = " Hello World!"
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/layout')
+def layout():
+    return render_template('layout.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
