81b14f29dfa7bc41bf86e68e9e8385593ef8a662
surabhinanhe <72373862+surabhinanhe@users.noreply.github.com>
2020-10-12 12:46:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template,request
+app = Flask(__name__)
+
+@app.route('/')
+def student():
+    return render_template('index.html')
+
+@app.route('/result',methods=['POST','GET'])
+def result():
+    if request.method=='POST':
+        result=request.form
+        return render_template('text.html', result=result)
+
+app.run(debug=True)
